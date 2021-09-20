15579b47cd041b3eadee776206849d6f6b9658f7
Beli Dev <wilwil29@hotmail.fr>
2021-09-21 00:21:49 +0200

    lobbyButton fixed, WCAG validation

--- frontend/js/pages/subscription.js
+++ frontend/js/pages/subscription.js
@@ -5,7 +5,6 @@ const password = document.getElementById("password").value
 console.log(password)
 
 let regexName = new RegExp(/^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ-]{1,30}$/);
-let regexAddress = new RegExp(/^[a-zA-Z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ ._\s-]{5,60}$/);
 let regexMail = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/);
 let regexPassword = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/);
 
